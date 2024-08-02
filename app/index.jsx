4049bcf645fca84d9f12943500d2836e5aa03e1b
i-roger <mroger.dev@gmail.com>
2024-08-02 18:35:03 -0300

    novo teste

---
+++ app/index.jsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import ColorList from '../components/ColorList'
+
+const Home = () => {
+  return (
+    <View>
+      <ColorList color="#e61919"/>
+    </View>
+  )
+}
+
+export default Home
---
+++ components/TabBar.jsx
@@ -0,0 +1,71 @@
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
+import React from 'react'
+
+const TabBar = ({ state, descriptors, navigation }) => {
+  return (
+<View style={styles.tabbar}>
+      {state.routes.map((route, index) => {
+        const { options } = descriptors[route.key];
+        const label =
+          options.tabBarLabel !== undefined
+            ? options.tabBarLabel
+            : options.title !== undefined
+            ? options.title
+            : route.name;
+
+        // Linha abaixo para corrigir o erro de uma rota que nao existe na TABBAR
+        if(['_sitemap', '+not-found'].includes(route.name)) return null;
+
+        const isFocused = state.index === index;
+
+        const onPress = () => {
+          const event = navigation.emit({
+            type: 'tabPress',
+            target: route.key,
+            canPreventDefault: true,
+          });
+
+          if (!isFocused && !event.defaultPrevented) {
+            navigation.navigate(route.name, route.params);
+          }
+        };
+
+        const onLongPress = () => {
+          navigation.emit({
+            type: 'tabLongPress',
+            target: route.key,
+          });
+        };
+
+        return (
+          <TouchableOpacity
+            accessibilityRole="button"
+            accessibilityState={isFocused ? { selected: true } : {}}
+            accessibilityLabel={options.tabBarAccessibilityLabel}
+            testID={options.tabBarTestID}
+            onPress={onPress}
+            onLongPress={onLongPress}
+            style={{ flex: 1 }}
+          >
+            <Text style={{ color: isFocused ? '#673ab7' : '#222' }}>
+              {label}
+            </Text>
+          </TouchableOpacity>
+        );
+      })}
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    tabbar: {
+        position: 'absolute',
+        bottom: 25,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        backgroundColor: 'white'
+    }
+})
+
+export default TabBar
